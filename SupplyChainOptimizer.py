58b606a1fe6ee6ce974cad3a9e7c7c5ab60c943b
alessiobar <47196616+alessiobar@users.noreply.github.com>
2021-05-15 00:37:52 +0200

    Update SupplyChainOptimizer.py

--- SupplyChainOptimizer.py
+++ SupplyChainOptimizer.py
@@ -1,38 +1,33 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 import numpy as np
-import os
-import matplotlib as plt
-os.chdir(r"C:\Users\alema\Downloads")
 import statistics
+import os
 
+#Dataset
+os.chdir(r"C:\Users\alema\Downloads")
+df = pd.read_csv("Supply_Chain_Shipment_Pricing_Data (1).csv")
+#print(df[x].value_counts()) 
 
-df1 = pd.read_csv("Supply_Chain_Shipment_Pricing_Data (1).csv")
-df = df1
-#print(df[x].value_counts())
-
-df['line item insurance (usd)']=0
-
-for x in df.columns: #should be only for categorical, so subset it to be good enough generalizable code..
-    try:
-        df[x][0] = int(df[x][0])
-    except Exception:
+#Replacing NA with the mode for categorical columns
+for x in df.columns: 
+    try: #this aims at determining the column type by attempting to cast the first element(s) 
+        for y in range(10): 
+            df[x][y] = int(df[x][y])
+    except Exception: #failed columns are most likely categorical, to be sure just set len(df[column]) instead of 10 in the range
         df.fillna(df[x].mode())
 
+#Rename some columns for easiness
 df.rename(columns = {'shipment mode':'mode', "scheduled delivery date":"date","line item quantity":"quantity", 'freight cost (usd)':"cost",'weight (kilograms)':"weight"}, inplace = True)
-df1 = df
-to_drop = []
 
+#Convert numerals in the weight column to integer format
 for x in range(len(df["id"])):
     try:
         df["weight"][x] = int(df["weight"][x])
     except Exception:
         pass
 
+#The only columns having engineered missing values where cost and weight, here non-fixable rows are dropped
+to_drop = []
 for e in ["weight","cost"]:
     for j in range(len(df[e])):
         try:
@@ -43,14 +38,14 @@ for e in ["weight","cost"]:
                 i=0
                 while str(row)[0]== "S":
                     i+=1
-                    if i==100:
+                    if i==100: 
                         print("infinite loop?")
                         break
                     the_id = df[e][j][df[e][j].find(":") + 1:df[e][j].find(")")]
                     row = df.iloc[[x for x in range(len(df["id"])) if df["id"][x] == int(the_id)][0]]
                 if str(row)[0]== "W" or str(row)[0]== "I" or str(row)[0]== "F" :
                     pass
-                else: # aka if it's a number, lo pluggo
+                else: 
                     df[e][j] = row[e]
 
         except Exception:
@@ -65,41 +60,34 @@ for e in ["weight","cost"]:
             if e=="weight":
                 df[e][j] = "placeholder"
 
-
+#Substituting ex-post the missing values with the overall weight_mean, computed ex-ante on the non missing values
 weight_mean = round(statistics.mean([x for x in df["weight"] if isinstance(x, int)]), 2)
 for x in range(len(df["cost"])):
     if df["weight"][x]=="placeholder":
         df["weight"][x] = weight_mean
 
-#to_drop = np.load("to_drop.npy")
+#Dropping elements added in the to_drop list
 to_drop = list(set(to_drop))
 for x in to_drop:
     df.drop(x, inplace=True)
 df = df.reset_index(drop=True)
 
+#Dropping id because it is useless for the analysis part
 df =df.drop(columns=["id"])
 
-#df.to_excel("basta.xlsx")
-#df1 = pd.read_excel("intermezzo.xlsx")
-
+#Replacing outliers with Q1 and Q3
 classes = ["cost",'weight', "cost",'pack price', 'unit price','unit of measure (per pack)','line item value']
-# for y in classes:
-#     for x in range(len(df["cost"])):
-#         try:
-#             df[y][x] = int(df["weight"][x])
-#         except Exception:
-#             pass
-
 for f in classes:
     q1, q3 = df[f].describe().values[4], df[f].describe().values[-2]
     iQR = q3 - q1
     for p in range(len(df["weight"])):
-        if df[f][p] <= q1-1.5*iQR or df[f][p]>= q3+1.5*iQR:
+        if df[f][p] <= q1-1.5*iQR or df[f][p]>= q3+1.5*iQR: #IQR method
             if df[f][p] <= q1-1.5*iQR:
                 df[f][p] = q1
             else:
                 df[f][p] = q3
 
+#Dummy Variables          
 df = pd.get_dummies(df, columns=['project code', 'pq #', 'po / so #', 'asn/dn #', 'country',
        'managed by', 'fulfill via', 'vendor inco term', 'mode',
        'pq first sent to client date', 'po sent to vendor date', 'date',
@@ -112,59 +100,47 @@ df = pd.get_dummies(df, columns=['project code', 'pq #', 'po / so #', 'asn/dn #'
 df["dosage"] = [df["dosage"].mode()[0] if isinstance(df["dosage"][x], float) else df["dosage"][x] for x in range(len(df["weight"]))]
 df = pd.get_dummies(df, columns=['dosage'])
 
+#Split into train and validation sets
 X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=["cost"]), df["cost"], test_size=0.33, random_state=42)
 
-#RF
+#Random Forest Model
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
-
+# Grid for GridSearchCV
 grid_rf = {'n_estimators': [200,1000],
                'max_depth': [10, 100],
                'min_samples_split':[2, 10],
                'min_samples_leaf': [1, 4],
-               'bootstrap': [True]}#, False]}
+               'bootstrap': [True, False]}
 
-rf = RandomForestRegressor(random_state = 42)#, n_estimators= 200, min_samples_split = 2, min_samples_leaf = 2, max_features ='auto', max_depth= 80, bootstrap= True)
-#rf.fit(X_train, y_train)
-
-#rf_random = rf
-something_rf = GridSearchCV(rf, param_grid=grid_rf, cv=5, verbose=2) # e no, io devo farla su tutto poi, per tunare a modo, non solo sullo split.
-something_rf.fit(df.drop(columns=["cost"]), df["cost"]) # e no devo tunare sulo su train split
+#GridSearch and Model Fit
+something_rf = GridSearchCV(rf, param_grid=grid_rf, cv=5, verbose=2)
+something_rf.fit(df.drop(X_train, y_train) 
 something_rf.best_params_
-
-#rf = RandomForestRegressor(random_state = 42, n_estimators= 200, min_samples_split =2, min_samples_leaf = 1, max_depth= 100, bootstrap= True)
-
 predictions = something.predict(X_test)
 errors = abs(predictions - y_test)
-mean_squared_error(y_test, predictions)
-mean_squared_error(y_test, predictions, squared=False)
-                                        #187273236.05130753 co 463 obs
-                                        #186722576.5434092
-                                        # 46421417.5825533 co 690 obs
-                                        # 46343852.48890569 wout normalization/scaling
-                                        #  6399807.202095908 co 690 obs retrainando
-                                        #  6399797.518667495 wout normalization
-                                        #39475272.091516696
-                                        # 5636978.805694842
-#                                        17794565.04079134
-                                        #17624560
+                 
+mean_squared_error(y_test, predictions, squared=True)
 mean_absolute_error(y_test, predictions)
-
-#retrain
-rf.fit(df.drop(columns=["cost"]), df["cost"])
-
 scores_rf = cross_val_score(RandomForestRegressor(random_state = 42, n_estimators= 1000, min_samples_split =2, min_samples_leaf = 1, max_depth= 100, bootstrap= True), df.drop(columns=["cost"]), df["cost"], cv=10 ) #df.drop(columns=["cost"]), df["cost"], cv=10)
-scores_rf.mean()
-
-####XGBOOST
+scores_rf.mean()       
+                 
+#rf = RandomForestRegressor(random_state = 42, n_estimators= 200, min_samples_split =2, min_samples_leaf = 1, max_depth= 100, bootstrap= True)
+                 
+#Extreme Gradient Boosting
+from xgboost import XGBRegressor
+from sklearn.model_selection import cross_val_score                 
 aaa=list(df.columns.values)
 i=0
-for a in aaa:
+for a in aaa: #remove strange characters for allowing xgb to work
     for char in ["[","]","<"]:
         aa = a.split(char)
         if len(aa)>1:
@@ -177,49 +153,35 @@ for a in aaa:
             aa = aa[0]
     aaa[i] = aa
     i += 1
-
+                 
 i=0
 for x in aaa:
     df.rename(columns = {list(df.columns.values)[i]: x}, inplace=True)
     i+=1
 
+#Remove duplicate columns that appearently appeared(?)
 duplicate_columns = df.columns[df.columns.duplicated()]
 df = df.loc[:,~df.columns.duplicated()]
 
-from xgboost import XGBRegressor
-from sklearn.model_selection import cross_val_score
+#Grid for XGBoost
 grid_xgb={'max_depth': [1, 3],
           'gamma': [1,10],
           'min_child_weight' : [1, 10],
           'n_estimators': [200,1000],
        }
 
-something_xgb = GridSearchCV(XGBRegressor(random_state=42), param_grid=grid_xgb, cv=5, verbose=2) # e no, io devo farla su tutto poi, per tunare a modo, non solo sullo split.
-something_xgb.fit(df.drop(columns=["cost"]), df["cost"]) #di nuovo no
+something_xgb = GridSearchCV(XGBRegressor(random_state=42), param_grid=grid_xgb, cv=5, verbose=2)
+something_xgb.fit(X_train, y_train) #df.drop(columns=["cost"]), df["cost"]
 something_xgb.best_params_
 
 scores_xgb = cross_val_score(XGBRegressor(random_state=42, max_depth=3, gamma=10, min_child_weight=1,n_estimators=1000), df.drop(columns=["cost"]), df["cost"], cv=10)
 scores_xgb.mean()
-
-something_xgb=XGBRegressor(random_state=42,max_depth=3, gamma=10, min_child_weight=1,n_estimators=1000)
-something_xgb.fit(X_train, y_train)
 predictions = something_xgb.predict(X_test)
 errors = abs(predictions - y_test)
 mean_squared_error(y_test, predictions)
 
-
-
-
- # with open('your_file.txt', 'w') as f:
- #     for item in olihhh: #list(df.columns.values):
- #         f.write("%s\n" % item)
-
-
-
-
-
+#Find top n features selected by the model
 xaz = list(something_xgb.feature_importances_)
-
 res=[]
 for x in range(10):
     m = max(xaz)
@@ -227,38 +189,25 @@ for x in range(10):
     i=0
     for y in xaz:
         if y == m:
-            print("gy")
             res.append((list(df.columns.values)[i],y))
             xaz.pop(i)
         i+=1
-
-
-
 pog.pop()
 pog = list(df.columns.values)
-
 asd = pd.DataFrame(xaz)
 asd.transpose()
 
-
-
-
-
-#refit di xgboost
+#Refit di xgboost on the whole initial dataset
 df111 = df
-
 df111.to_numpy()
 something_xgb.fit(df111.drop(columns=["cost"]), df111["cost"])
 
+#Optimization Function applied to shipment mode and constructed for XGBoost
 for y in range(100):
-    print("--------------")
     for x in ["mode_Air", "mode_Air Charter", "mode_Ocean", "mode_Truck"]:
-        #print(x)
         new_ob = df111.iloc[y]
-        #print(new_ob[x])
         new_ob["mode_Air"], new_ob["mode_Air Charter"], new_ob["mode_Ocean"], new_ob["mode_Truck"] = 0,0,0,0
         new_ob[x] = 1
-        #print(new_ob[x])
         original = new_ob["cost"]
         new_ob = new_ob.drop("cost")
         new_ob = new_ob.to_frame()
@@ -272,105 +221,14 @@ for y in range(100):
         predictions=something_xgb.predict(new_ob)
         print(predictions)
 
-
-
-
-
-for x in range(100):
-
-    new_ob["mode_Truck"] = 0
-    new_ob["mode_Air"] = 1
-    yy = new_ob["cost"]
-    new_ob = new_ob.drop("cost")
-    predictions = something_xgb.predict([new_ob])
-    errors = abs(predictions - yy)
-    print(errors)
-
-predictions
-new_ob["mode_Truck"] = 1
-new_ob["mode_Air"] = 0
-
-df.to_excel("witholeh.xlsx")
-
-def optimizer(obs):
-    truevalue = obs["cost"]
-    obs = obs.drop("cost")
-    ante_pred = rf_random.predict([obs])
-    print(ante_pred)
-    df["mode_Air"] = int(input())
-    df["mode_Truck"] = int(input())
-    post_pred = rf_random.predict([obs])
-    if ante_pred > post_pred:
-        return obs
-    else:
-        return new_obs
-
-print(optimizer(df.iloc[0]))
-
-#################################
-import matplotlib.pyplot as plt
-X_grid = [1,2,3,4,5]
-
-# Scatter plot for original data
-plt.scatter(y_test, predictions, color='blue')
-
-# plot predicted data
-plt.plot(X_grid, regressor.predict(X_grid),
-         color='green')
-plt.title('Random Forest Regression')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# rectangular box plot
-bplot1 = ax1.boxplot(df1,
-                     vert=True,  # vertical box alignment
-                     patch_artist=True,  # fill with color
-                     labels=["1","2","3","4"])  # will be used to label x-ticks
-
-ax1.set_title('Rectangular box plot')
-plt.show()
-
-
-
-
-from plotnine import * #attento al df1
-(
-    ggplot(df1)
-    + geom_boxplot(aes(x="mode", y='cost'))
-    + scale_x_discrete(labels=["Air","Truck","Air Charter", "Ocean"], name='boh')  # change ticks labels on OX
-)
-
-
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-
-x = ['Air', 'Air Charter', 'Ocean', 'Truck']
-energy = [393, 677, 465, 409]
-
-x_pos = [i for i, _ in enumerate(x)]
-
-plt.bar(x_pos, energy, color=["blue","darkblue","red","darkred"])
-plt.xlabel("Shipment Mode")
-plt.ylabel("Predicted Freight Cost")
-plt.title("Freight Cost Optimization")
-
-plt.xticks(x_pos, x)
-
-
-plt.show()
-
-
-
-data=df[["mode_Ocean","country_Namibia","cost","weight"]].corr()
-import pandas as pd
+# Some Plots
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-
-
+data=df[["mode_Ocean","country_Namibia","cost","weight"]].corr()
 dfe = pd.DataFrame(data,columns=['mode_Ocean','country_Namibia','cost',"weight"])
-
 corrMatrix = dfe.corr()
 sn.heatmap(corrMatrix, annot=True, )
 plt.show()
+
+#Group 11                
